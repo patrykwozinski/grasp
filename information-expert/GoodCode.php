49109614d1ae121e143ee82d5a4e8f9877e523ac
Patryk Woziński <patryk.wozinski@gmail.com>
2019-03-12 20:45:53 +0100

    ie good example

--- information-expert/GoodCode.php
+++ information-expert/GoodCode.php
@@ -1,14 +1,44 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: freeq
- * Date: 2019-03-12
- * Time: 20:41
- */
 declare(strict_types=1);
 
-
-final class GoodCode
+final class Price
 {
+	private $possibleUpdate;
+	private $amount;
 
+	public function __construct(bool $possibleUpdate, float $amount)
+	{
+		$this->possibleUpdate = $possibleUpdate;
+		$this->amount         = $amount;
+	}
+
+	public function canUpdate(): bool
+	{
+		return $this->possibleUpdate;
+	}
+
+	public function lastAmount(): float
+	{
+		return $this->amount;
+	}
+
+	public function multiply(int $multiplier): self
+	{
+		if (true === $this->possibleUpdate)
+		{
+			$newAmount = $this->amount * $multiplier;
+
+			return new self($this->possibleUpdate, $newAmount);
+		}
+
+		return $this;
+	}
+}
+
+final class UpdatePrice
+{
+	public function update(Price $price, int $multiplier): Price
+	{
+		return $price->multiply($multiplier);
+	}
 }
