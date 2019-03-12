111db233e1cbab1688dd2cf523dfbebb81e33716
Patryk Woziński <patryk.wozinski@gmail.com>
2019-03-12 21:19:13 +0100

    lc good code example

--- low-coupling/GoodCode.php
+++ low-coupling/GoodCode.php
@@ -2,7 +2,30 @@
 declare(strict_types=1);
 
 
-final class GoodCode
+interface Neighbor
 {
+	public function saysHello(): void;
+}
 
+final class Alpha
+{
+	public function withNeighbor(Neighbor $neighbor): self
+	{
+		$this->neighbor = $neighbor;
+	}
+
+	public function whoIsYourNeighbor(): Neighbor
+	{
+		return $this->neighbor;
+	}
+
+	public function sayHelloToNeighbor(Neighbor $neighbor): void
+	{
+		$neighbor->saysHello();
+	}
+}
+
+final class Beta implements Neighbor
+{
+	public function saysHello(): void {}
 }
