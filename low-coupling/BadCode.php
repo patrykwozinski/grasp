dc16c6d63866afc7a1fbf00d4c604eb56618dd1a
Patryk Woziński <patryk.wozinski@gmail.com>
2019-03-12 21:17:46 +0100

    lc bad-code

--- low-coupling/BadCode.php
+++ low-coupling/BadCode.php
@@ -2,7 +2,25 @@
 declare(strict_types=1);
 
 
-final class BadCode
+final class Alpha
 {
+	public function withNeighbor(Beta $beta): self
+	{
+		$this->neighbor = $beta;
+	}
 
+	public function whoIsYourNeighbor(): Beta
+	{
+		return $this->neighbor;
+	}
+
+	public function sayHelloToNeighbor(Beta $beta): void
+	{
+		$beta->betaSaysHello();
+	}
+}
+
+final class Beta
+{
+	public function betaSaysHello(): void {}
 }
